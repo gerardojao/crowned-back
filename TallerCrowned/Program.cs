b2cdf450abd3444af648892041d530a4d8c3e577
Gerardo Arrieta Ortiz <gerardojao@gmail.com>
2026-05-26 09:07:33 +0200

    cmabios en nombre de solucion

--- TallerCrowned/Program.cs
+++ TallerCrowned/Program.cs
@@ -129,7 +129,7 @@ builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FamilyApp API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Taller Crowned API", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
